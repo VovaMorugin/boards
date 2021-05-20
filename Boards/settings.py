619c30b8b6ef095e937fc448bea19c3390c53eb7
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-05-20 10:02:55 -0700

    Initial commit

--- Boards/settings.py
+++ Boards/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+from dotenv import load_dotenv
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'g5+exyh#cx!7*$7nb5g5fzmubb@uoimlksme&e5xqe!(z!sr^*'
