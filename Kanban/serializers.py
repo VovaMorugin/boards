61938d62e41b3a233ebb1fb2d275ee645f40e0b0
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-06-21 23:43:36 -0700

    add rest_framework and start adding serializers for future API

---
+++ Kanban/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Board
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = '__all__'
+    
